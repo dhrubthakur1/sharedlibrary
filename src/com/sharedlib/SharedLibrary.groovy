f4e29f1d3c854fe8c2696307f8ed540ef3492228
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-17 15:26:15 +0530

    Update SharedLibrary.groovy

--- src/com/sharedlib/SharedLibrary.groovy
+++ src/com/sharedlib/SharedLibrary.groovy
@@ -4,10 +4,12 @@ public class SharedLibrary {
   def steps
   
   public SharedLibrary(steps) {
+    println('111')
     this.steps = steps
   }
   
   public void startBuild() {
+    println('222')
     println(steps)
     steps.sh "echo Hello World!"
     //steps.echo "Hello, World!"
